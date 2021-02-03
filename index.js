187733151f0574685b46f72c8d90ab36d76f551c
Kirill <uper99@gmail.com>
2021-02-04 01:31:16 +0300

    added files

---
+++ index.js
@@ -0,0 +1,60 @@
+let fruits = [
+    {id: 1, title: 'Яблоки', price: 20, img: 'https://e1.edimdoma.ru/data/ingredients/0000/2374/2374-ed4_wide.jpg?1487746348'},
+    {id: 2, title: 'Апельсины', price: 30, img: 'https://m.dom-eda.com/uploads/images/catalog/item/dfc9a3e974/3cbf3bd41c_1000.jpg'},
+    {id: 3, title: 'Манго', price: 40, img: 'https://st3.depositphotos.com/1020804/12760/i/600/depositphotos_127608560-stock-photo-mango-cubes-and-mango-fruit.jpg'},
+]
+
+const toHTML = fruit => `
+    <div class="col">
+        <div class="card" style="width: 210px;">
+            <img class="card-img-top" style="height: 200px;" src="${fruit.img}" alt="${fruit.title}">
+            <div class="card-body">
+                <h5 class="card-title">${fruit.title}</h5>
+                <a href="#" class="btn btn-primary" data-btn="price" data-id="${fruit.id}">Посмотреть цену</a>
+                <a href="#" class="btn btn-danger" data-btn="remove" data-id="${fruit.id}">Удалить</a>
+            </div>
+        </div>
+    </div>
+`
+
+function render() {
+    const html = fruits.map(toHTML).join('')
+    document.querySelector('#fruits').innerHTML = html
+}
+
+render()
+
+const priceModal = $.modal({
+    title: 'Цена товара',
+    closable: true,
+    width: '400px',
+    footerButtons: [
+        {text: 'Закрыть', type: 'primary', handler() {
+            priceModal.close()
+        }}
+    ]
+})
+
+document.addEventListener('click', event => {
+    event.preventDefault()
+    const btnType = event.target.dataset.btn
+    const id = +event.target.dataset.id
+    const fruit = fruits.find(f => f.id === id)
+
+    if (btnType === 'price') {
+        priceModal.setContent(`
+            <p>Цена на ${fruit.title}: <strong>${fruit.price} рублей</strong></p>
+        `)
+        priceModal.open()
+    } else if (btnType === 'remove') {
+        $.confirm({
+            title: 'Вы уверены?',
+            content: `<p>Вы удаляете данный фрукт</p>`
+        }).then(() => {
+            fruits = fruits.filter(f => f.id !== id)
+            render()
+        }).catch(() => {
+            console.log('cancel')
+        })
+    }
+})
